d39797dcf071d5fa37362408ac91197d5d926d4d
timwis <tim@timwis.com>
2016-07-25 07:26:10 -0400

    add remaining service areas

--- src/components/service-areas.js
+++ src/components/service-areas.js
@@ -13,22 +13,22 @@ module.exports = (ais) => {
             <div class="panel mbm">
               <h3>School Catchment</h3>
               <h4 class="alternate">Elementary School</h4>
-              <strong data-hook="elementary-school">${props.elementary_school}</strong>
+              <strong>${props.elementary_school}</strong>
               <h4 class="alternate">Middle School</h4>
-              <strong data-hook="middle-school">${props.middle_school}</strong>
+              <strong>${props.middle_school}</strong>
               <h4 class="alternate">High School</h4>
-              <strong data-hook="high-school">${props.high_school}</strong>
+              <strong>${props.high_school}</strong>
             </div>
           </div>
           <div class="medium-12 columns">
             <div class="panel mbm">
               <h3>Political</h3>
               <h4 class="alternate">2016 Councilmanic District</h4>
-              <strong data-hook="council-district"></strong>
+              <strong>${props.council_district_2016}</strong>
               <h4 class="alternate">Ward</h4>
-              <strong data-hook="ward"></strong>
+              <strong>${props.political_ward}</strong>
               <h4 class="alternate">Ward Division</h4>
-              <strong data-hook="ward-divisions"></strong>
+              <strong>${props.political_division}</strong>
             </div>
           </div>
         </div>
@@ -40,17 +40,15 @@ module.exports = (ais) => {
               <div class="row">
                 <div class="medium-12 columns">
                   <h4 class="alternate">Police District</h4>
-                  <strong data-hook="police-district"></strong>
-
+                  <strong>${props.police_district}</strong>
                   <h4 class="alternate">Police Sector</h4>
-                  <strong data-hook="police-sector"></strong>
+                  <strong>${props.police_sector}</strong>
                 </div>
                 <div class="medium-12 columns">
                   <h4 class="alternate">Police Public Service Area</h4>
-                  <strong data-hook="police-psa"></strong>
-
+                  <strong>${props.police_service_area}</strong>
                   <h4 class="alternate">Police Division</h4>
-                  <strong data-hook="police-division"></strong>
+                  <strong>${props.police_division}</strong>
                 </div>
               </div>
             </div>
@@ -64,30 +62,29 @@ module.exports = (ais) => {
               <div class="row">
                 <div class="medium-12 columns">
                   <h4 class="alternate">Highway District</h4>
-                  <strong data-hook="highway-district"></strong>
+                  <strong>${props.highway_district}</strong>
                   <h4 class="alternate">Highway Section</h4>
-                  <strong data-hook="highway-section"></strong>
+                  <strong>${props.highway_section}</strong>
                   <h4 class="alternate">Highway Subsection</h4>
-                  <strong data-hook="highway-subsection"></strong>
+                  <strong>${props.highway_subsection}</strong>
                   <h4 class="alternate">Street Light Routes</h4>
-                  <strong data-hook="street-light-routes"></strong>
+                  <strong>${props.street_light_route}</strong>
                   <h4 class="alternate">Traffic District</h4>
-                  <strong data-hook="traffic-district"></strong>
+                  <strong>${props.traffic_district}</strong>
                   <h4 class="alternate">Traffic PM District</h4>
-                  <strong data-hook="traffic-pm-district"></strong>
+                  <strong>${props.traffic_pm_district}</strong>
                 </div>
                 <div class="medium-12 columns">
                   <h4 class="alternate">Trash & Recycling Day</h4>
-                  <strong data-hook="rubbish-day"></strong>
+                  <strong>${props.rubbish_recycle_day}</strong>
                   <h4 class="alternate">Leaf Collection Day</h4>
-                  <strong data-hook="leaf-collection"></strong>
-                  <strong data-hook="" class="h1 stat no-margin"></strong>
+                  <strong>${props.leaf_collection_area}</strong>
                   <h4 class="alternate">Recycling Diversion Rate</h4>
-                  <strong data-hook="recycling-diversion"></strong>
+                  <strong>${props.recycling_diversion_rate}</strong>
                   <h4 class="alternate">Sanitation Area</h4>
-                  <strong data-hook="sanitation-area"></strong>
+                  <strong>${props.sanitation_area}</strong>
                   <h4 class="alternate">Sanitation District</h4>
-                  <strong data-hook="sanitation-district"></strong>
+                  <strong>${props.sanitation_district}</strong>
                 </div>
               </div>
             </div>
@@ -99,24 +96,24 @@ module.exports = (ais) => {
             <div class="panel mbm">
               <h3>Districts</h3>
               <h4 class="alternate">Planning</h4>
-              <strong data-hook="planning"></strong>
+              <strong>${props.planning_district}</strong>
               <h4 class="alternate">Licenses and Inspections (L+I)</h4>
-              <strong data-hook="li-district"></strong>
+              <strong>${props.li_district}</strong>
               <h4 class="alternate">Recreation</h4>
-              <strong data-hook="recreation"></strong>
+              <strong>${props.recreation_district}</strong>
             </div>
           </div>
           <div class="medium-12 columns">
             <div class="panel mbm">
               <h3>Water</h3>
               <h4 class="alternate">PWD Maintenance Districts</h4>
-              <strong data-hook="pwd-maintenance"></strong>
+              <strong>${props.pwd_maint_district}</strong>
               <h4 class="alternate">PWD Pressure Districts</h4>
-              <strong data-hook="pwd-pressure"></strong>
+              <strong>${props.pwd_pressure_district}</strong>
               <h4 class="alternate">Water Treatment Plant</h4>
-              <strong data-hook="water-treatment"></strong>
+              <strong>${props.pwd_treatment_plant}</strong>
               <h4 class="alternate">Water Plate Index</h4>
-              <strong data-hook="water-plate"></strong>
+              <strong>${props.pwd_water_plate}</strong>
             </div>
           </div>
         </div>
